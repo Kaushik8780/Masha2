acc6680789eb1bfd4ff435e07a264ba8d0d963d0
Kaushik8780 <solankikaushik8780@gmail.com>
2026-05-21 22:40:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+zOKfJgE4A9QzNzA1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_bots')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 't.me/Kaushiksolanki2955')
+OWNER_LNK = environ.get('OWNER_LNK', 't.me/Solanki2955')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
